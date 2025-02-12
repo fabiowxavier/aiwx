25f7bc7455fec20aa57bf742228966eca91c3e97
Fabio <fabio@Adeles-MacBook-Pro.local>
2025-02-12 15:37:41 +0000

    add input to vite.config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,13 @@ export default {
     root: '.', // Ensure index.html is at the root
     build: {
         outDir: 'dist', // Output folder for production
+        rollupOptions: {
+            input: {
+                main: 'index.html',
+                about: 'about.html',
+                contact: 'contact.html'
+            }
+        }
     },
     server: {
         port: 3000,
