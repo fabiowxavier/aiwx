55d48698d3e098a1484ba755fae4b4cd5b58808f
Fabio <fabio@Adeles-MacBook-Pro.local>
2025-02-12 10:04:24 +0000

    Add existing project files

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    './about.html',
+    './contact.html',
+    './src/**/*.{html,js}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
---
+++ vite.config.js
@@ -0,0 +1,9 @@
+export default {
+    root: '.', // Tells Vite your index.html is in this root directory
+    build: {
+        outDir: 'dist' // Output folder for production
+    },
+    server: {
+        port: 3000 // Dev server port (optional)
+    }
+}
