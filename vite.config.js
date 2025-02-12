383b40eaedd7ee1bd486668c4c419a797367110c
Fabio <fabio@Adeles-MacBook-Pro.local>
2025-02-12 11:33:25 +0000

    Adjust configurations and fix paths in tailwind.config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 export default {
     root: '.', // Tells Vite your index.html is in this root directory
     build: {
-        outDir: 'dist' // Output folder for production
+        outDir: 'dist', // Output folder for production
+        publicDir: 'src', // Adjust this to where your static files should be served from
     },
     server: {
         port: 3000 // Dev server port (optional)
