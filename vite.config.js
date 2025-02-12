b1393e6dda2b17c0108b4601ffc37fec696208ca
Fabio <fabio@Adeles-MacBook-Pro.local>
2025-02-12 11:44:02 +0000

    Move header and footer to public folder

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ export default {
     root: '.', // Tells Vite your index.html is in this root directory
     build: {
         outDir: 'dist', // Output folder for production
-        publicDir: 'src/components', // Adjust this to where your static files should be served from
+        publicDir: '/components', // Adjust this to where your static files should be served from
     },
     server: {
         port: 3000 // Dev server port (optional)
