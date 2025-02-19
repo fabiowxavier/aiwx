4fd02a6e0466c3b7feef06cbf27f36f7e1df9fe7
Fabio <fabio@Adeles-MacBook-Pro.local>
2025-02-19 23:34:19 +0000

    rearrange cards on carousel and add content to it

--- src/main.js
+++ src/main.js
@@ -25,7 +25,7 @@ document.addEventListener("DOMContentLoaded", function () {
         touchControls: true,
         gyroControls: false,
         minHeight: 200.00,
-        minWidth: 600.00,
+        minWidth: 200.00,
         scale: 1.00,
         scaleMobile: 1.00,
         color: 0x18528a,         // Custom color
